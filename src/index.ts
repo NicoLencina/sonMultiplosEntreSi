889b64694be82647b3a559a716ee4e843e9d4396
Lencina Nicolas Angelo <104397441+NicoLencina@users.noreply.github.com>
2022-05-20 13:12:29 -0300

    ok problema X % Y, estava el numero grande siempre adelante , solucion Y % X

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 let dato1,
   dato2: number = 0;
-let validador: boolean = true;
+let validador: number = 0;
 
-function esMultiplo(x: number, y: number): boolean {
-  let resul: number = y % x;
-  if (resul === 0) {
-    return (validador = true);
-  } else {
-    return (validador = false);
-  }
+function esMultiplo(x: number, y: number): number {
+  validador = y % x;
+  return validador;
 }
 
 dato1 = Number(prompt("Ingrese el primer Numero"));
@@ -17,9 +13,9 @@ dato2 = Number(
 );
 
 esMultiplo(dato1, dato2);
-console.log(validador);
-if (validador === true) {
-  console.log("El numero " + dato2 + " es multiplo de " + dato1);
+
+if (validador === 0) {
+  console.log("El numero: " + dato1 + " es multiplo de: " + dato2);
 } else {
   console.log("No son Multiplos");
 }
